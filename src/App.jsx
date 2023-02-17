412c268bb81bd0838f26a99070d8aa19f54131ae
RickyAnchores <ricky23anchores@gmail.com>
2023-02-17 21:06:57 +0000

    first styling

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Components/Nav/Nav';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Nav />
+      <h3>rickydatadev</h3>
+      <p>Portfolio for Data Tech and Dev</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='Nav'>
+        <h3>Nav</h3>
+        <p>navlist</p>
+    </div>
+  )
+}
+
+export default Nav
