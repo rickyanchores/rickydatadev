f5139629f99d5dd3efafe37741242eb2b45fbed6
RickyAnchores <ricky23anchores@gmail.com>
2023-02-19 13:36:41 +0000

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Nav from './Components/Nav/Nav';
+import Home from './Pages/Home/Home';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 const App = () => {
   return (
     <div className='App'>
-      <Nav />
-      <h3>rickydatadev</h3>
-      <p>Portfolio for Data Tech and Dev</p>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route exact component={Home}/>
+        </Routes>
+        <Home />
+      </Router>
     </div>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
+import {HashLink as Link} from "react-router-hash-link";
+
 
 const Nav = () => {
+
+  const navList = [
+    {
+      name: "Home",
+      href: "/"
+    },
+    {
+      name: "Projects",
+      href: "/projects"
+    },
+    {
+      name: "About",
+      href: "/about"
+    }
+  ] 
+  
   return (
     <div className='Nav'>
         <h3>Nav</h3>
-        <p>navlist</p>
+        <div className="navLinks">
+        {navList.map((link) => {
+          return(
+            <li>
+              <Link className="link" to={link.href}>{link.name}</Link>
+            </li>
+          )
+        })}
+        </div>
     </div>
   )
 }
 
-export default Nav
+export default Nav;
